d191f438222c7a5505b3cded1e7295cdbe02d186
jlowb <nederaalex@gmail.com>
2023-11-01 00:35:40 -0400

    prelim lambda functions, need to check tmr

--- swiftify-backend/src/main/java/com/group113/swiftify/DependencyFactory.java
+++ swiftify-backend/src/main/java/com/group113/swiftify/DependencyFactory.java
@@ -1,26 +1,24 @@
-
 package com.group113.swiftify;
 
-import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
-import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
-import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.s3.S3Client;
 
-/**
- * The module containing all dependencies required by the {@link App}.
- */
 public class DependencyFactory {
 
-    private DependencyFactory() {}
+    private static DynamoDbClient dynamoDbClient = null;
+    private static S3Client s3Client = null;
 
-    /**
-     * @return an instance of S3Client
-     */
+    public static DynamoDbClient dynamoDbClient() {
+        if (dynamoDbClient == null) {
+            dynamoDbClient = DynamoDbClient.builder().build();
+        }
+        return dynamoDbClient;
+    }
+
     public static S3Client s3Client() {
-        return S3Client.builder()
-                       .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
-                       .region(Region.US_EAST_1)
-                       .httpClientBuilder(UrlConnectionHttpClient.builder())
-                       .build();
+        if (s3Client == null) {
+            s3Client = S3Client.builder().build();
+        }
+        return s3Client;
     }
 }
