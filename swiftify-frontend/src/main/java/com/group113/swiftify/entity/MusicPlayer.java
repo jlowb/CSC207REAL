2fed5533b6a3ab77220645578fea812bb39f238f
theardentomicron <g.okonkwo@mail.utoronto.ca>
2023-11-17 16:03:09 -0500

    template of music player

--- swiftify-frontend/src/main/java/com/group113/swiftify/entity/MusicPlayer.java
+++ swiftify-frontend/src/main/java/com/group113/swiftify/entity/MusicPlayer.java
@@ -1,8 +1,10 @@
 package com.group113.swiftify.entity;
 
+import jdk.jshell.spi.ExecutionControl;
+
+import java.util.Stack;
+
 public class MusicPlayer {
-
-
     // core logic of "music list"
     // music chain is by default an empty stack. the top of the stack is the current song.
     // hence, the next song is done by adding to the top, prev song is done by removing from the top (history).
@@ -11,23 +13,59 @@ public class MusicPlayer {
     // if no button is pressed and not shuffled, nextsong will be song with next song id.
     // if no button and shuffled, nextsong gives random id not previously visited.
 
-    // todo:
-    // implement MusicChain stack thing as an inner class (pop methods, etc.) -------- Greatman
-    //      make sure to include construction of MusicChain inside of MusicPlayer initializer.
-    // implement next/prev/etc. logic -------- Greatman
-    // play/pause -------- Alex
+    private class MusicChain {
+        private final Stack<Integer> stack;
 
-    // attribute:
-    // bool playing
-    // bool shuffled
-    // musicChain - current playlist being played
+        public MusicChain() {
+            this.stack = new Stack<Integer>();
+        }
 
-    // methods:
-    // MusicPlayer -> constructor
-    // Song getCurrentSong -> looks at the top of the stack
-    // play -> run getCurrentSong, provide Song to PlayerState as input
-    // pause -> interact with PlayerState, does not need to provide input
-    // nextSong -> see above logic
-    // prevSong -> pops stack
-    // void toggleShuffle
+        public int top() {
+            return stack.peek();
+        }
+
+        public void add(int songId) {
+            stack.add(songId);
+        }
+
+        public void pop() {
+            stack.pop();
+        }
+
+    }
+
+    private boolean playing;
+    private boolean shuffled;
+    private MusicChain musicChain;
+
+    public MusicPlayer(boolean playing, boolean shuffled) {
+        this.playing = playing;
+        this.shuffled = shuffled;
+        this.musicChain = new MusicChain();
+    }
+
+    public int getCurrentSong() {
+        return musicChain.top();
+    }
+
+    public void play() {
+
+    }
+
+    public void pause() {
+
+    }
+
+    public void nextSong() {
+
+    }
+
+    public void prevSong() {
+
+    }
+
+    public void toggleShuffle() {
+        this.shuffled = !this.shuffled;
+    }
+
 }
