2243ab356544f5743f8657cc76b4aeda744d1fae
malaikamalik <malaika.malik@mail.utoronto.ca>
2023-11-29 01:33:10 -0500

    Adding the GUI View Deluxe Albums Play

--- swiftify-frontend/src/view/TaylorVersion.java
+++ swiftify-frontend/src/view/TaylorVersion.java
@@ -9,28 +9,41 @@ import java.beans.PropertyChangeListener;
 import java.util.Objects;
 
 public class TaylorVersion extends JFrame implements ActionListener, PropertyChangeListener {
-    private JButton FearlessButton;
-    private JButton RedButton;
-    private JButton BackButton;
-    private JPanel FearlessPanel;
-    private JPanel RedPanel;
+    private JButton fearlessButton;
+    private JButton redButton;
+    private JButton backButton;
+    private JPanel fearlessPanel;
+    private JPanel redPanel;
+    private JPanel bottomPanel;
+    private JButton playButton;
+    private JButton pauseButton;
+    private JButton forwardButton;
 
     public TaylorVersion() {
         createUIComponents();
 
+        JLabel statusLabel = new JLabel("Playing Now: ");
+
         JPanel panel = new JPanel(new BorderLayout());
         panel.setBackground(Color.BLACK);
 
-        FearlessPanel.setLayout(new BorderLayout());
-        RedPanel.setLayout(new BorderLayout());
+        fearlessPanel.setLayout(new BorderLayout());
+        redPanel.setLayout(new BorderLayout());
 
-        panel.add(FearlessPanel, BorderLayout.WEST);
-        panel.add(RedPanel, BorderLayout.EAST);
+        panel.add(fearlessPanel, BorderLayout.WEST);
+        panel.add(redPanel, BorderLayout.EAST);
 
+        fearlessPanel.add(fearlessButton, BorderLayout.SOUTH);
+        redPanel.add(redButton, BorderLayout.SOUTH);
 
-        FearlessPanel.add(FearlessButton, BorderLayout.SOUTH);
-        RedPanel.add(RedButton, BorderLayout.SOUTH);
+        // Create the bottom panel with buttons
+        bottomPanel = new JPanel(new FlowLayout());
+        bottomPanel.add(backButton);
+        bottomPanel.add(playButton);
+        bottomPanel.add(pauseButton);
+        bottomPanel.add(forwardButton);
 
+        panel.add(bottomPanel, BorderLayout.SOUTH);
 
         setContentPane(panel);
         setTitle("Swiftify - Albums - Taylor's Version");
@@ -38,8 +51,6 @@ public class TaylorVersion extends JFrame implements ActionListener, PropertyCha
         setSize(1080, 680);
         setLocationRelativeTo(null);
         setVisible(true);
-
-
 
         ActionListener actionListener = new ActionListener() {
             @Override
@@ -51,52 +62,60 @@ public class TaylorVersion extends JFrame implements ActionListener, PropertyCha
                     if (Objects.equals(e.getActionCommand(), "Red (Taylor's Version)")) {
                         System.out.println("red");
                     }
+
                 }
             }
         };
 
-        FearlessButton.addActionListener(actionListener);
-        RedButton.addActionListener(actionListener);
+        fearlessButton.addActionListener(actionListener);
+        redButton.addActionListener(actionListener);
+
     }
 
     public static void main(String[] args) {
         new TaylorVersion();
     }
 
-
     private void createUIComponents() {
-        FearlessPanel = new TaylorVersion.ImagePanel("/Users/malaikamalik/Desktop/malik130/CSC207REAL/swiftify-frontend/src/pngs/fearless.png");
-        RedPanel = new TaylorVersion.ImagePanel("/Users/malaikamalik/Desktop/malik130/CSC207REAL/swiftify-frontend/src/pngs/red.png");
+        fearlessPanel = new ImagePanel("/Users/malaikamalik/Desktop/malik130/CSC207REAL/swiftify-frontend/src/pngs/fearless.png");
+        redPanel = new ImagePanel("/Users/malaikamalik/Desktop/malik130/CSC207REAL/swiftify-frontend/src/pngs/red.png");
 
-        // Initialize buttons
-        FearlessButton = new JButton("Fearless (Taylor's Version)");
-        RedButton = new JButton("Red (Taylor's Version)");
-        FearlessPanel.setPreferredSize(new Dimension(530, 340));
-        RedPanel.setPreferredSize(new Dimension(530, 340));
+        fearlessButton = new JButton("Fearless (Taylor's Version)");
+        redButton = new JButton("Red (Taylor's Version)");
+        playButton = new JButton(">");
+        pauseButton = new JButton("||");
+        forwardButton = new JButton("<");
+        JLabel statusLabel = new JLabel("Playing Now");
 
 
-        FearlessButton.setPreferredSize(new Dimension(150, 50));
-        RedButton.setPreferredSize(new Dimension(150, 50));
+
+        fearlessPanel.setPreferredSize(new Dimension(530, 340));
+        redPanel.setPreferredSize(new Dimension(530, 340));
+
+        fearlessButton.setPreferredSize(new Dimension(150, 50));
+        redButton.setPreferredSize(new Dimension(150, 50));
+        statusLabel.setPreferredSize(new Dimension(300, 20));
+        playButton.setPreferredSize(new Dimension(200, 50));
+        pauseButton.setPreferredSize(new Dimension(200, 50));
+        forwardButton.setPreferredSize(new Dimension(200, 50));
     }
-
-
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
-
+        // Handle property changes if needed
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        // Handle action events if needed
     }
+
     static class ImagePanel extends JPanel {
         private Image backgroundImage;
 
         public ImagePanel(String imagePath) {
             this.backgroundImage = new ImageIcon(imagePath).getImage();
             setPreferredSize(new Dimension(backgroundImage.getWidth(this), backgroundImage.getHeight(this)));
-
 
             setBackground(new Color(99, 93, 133));
         }
@@ -121,5 +140,3 @@ public class TaylorVersion extends JFrame implements ActionListener, PropertyCha
         }
     }
 }
-
-
