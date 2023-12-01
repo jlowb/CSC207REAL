aaf7329e45fba051220a249d4e54aea127662b9b
malaikamalik <malaika.malik@mail.utoronto.ca>
2023-12-01 15:27:32 -0500

    adding back button

---
+++ swiftify-frontend/src/view/loading_album.java
@@ -0,0 +1,46 @@
+package view;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class loading_album extends JFrame {
+    private JPanel panel1;
+    private JButton button1;
+    private JProgressBar progressBar1;
+    private JPanel panel2;
+    private JButton button2;
+    private JButton button3;
+    private JButton button12;
+    private JScrollBar scrollBar1;
+    private JButton songButton;
+
+
+    public loading_album(String selectedAlbum) {
+        setContentPane(panel1);
+        setTitle("Swiftify Album- Red");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(1080, 680);
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+        ActionListener a = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (e.getSource() instanceof JButton) {
+                    LoadAlbumView1 page = new LoadAlbumView1();
+                    page.setVisible(true);
+                }
+                dispose();
+            }
+        };
+
+    }
+    public static void main(String[] args) {
+        new loading_album(args[0]);
+    }
+
+
+}
+
+
