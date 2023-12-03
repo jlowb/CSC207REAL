8232ce3a2d58d5fccba72ed781d7496347b4a6a3
theardentomicron <g.okonkwo@mail.utoronto.ca>
2023-12-03 18:00:17 -0500

    fixed merge issues, blah blah

--- swiftify-frontend/src/view/LoadSongsView.java
+++ swiftify-frontend/src/view/LoadSongsView.java
@@ -1,7 +1,6 @@
 package view;
 
 import data_access.URLSongLoader;
-import data_access.URLSongLoader;
 import entity.MusicPlayerFacade;
 import entity.Song;
 import entity.SongButton;
@@ -16,33 +15,21 @@ import interface_adapter.next_song.NextSongController;
 import interface_adapter.next_song.NextSongPresenter;
 import interface_adapter.next_song.NextSongViewModel;
 import interface_adapter.pause_song.PauseSongController;
-import interface_adapter.pause_song.PauseSongPresenter;
-import interface_adapter.pause_song.PauseSongViewModel;
 import interface_adapter.play_song.PlaySongController;
-import interface_adapter.play_song.PlaySongPresenter;
-import interface_adapter.play_song.PlaySongViewModel;
 import interface_adapter.prev_song.PrevSongController;
 import interface_adapter.prev_song.PrevSongPresenter;
 import interface_adapter.prev_song.PrevSongViewModel;
 import interface_adapter.resume_song.ResumeSongController;
-import interface_adapter.resume_song.ResumeSongPresenter;
-import interface_adapter.resume_song.ResumeSongViewModel;
 import javazoom.jl.decoder.JavaLayerException;
 import use_case.next_song.NextSongInputBoundary;
 import use_case.next_song.NextSongInputData;
 import use_case.next_song.NextSongInteractor;
-import use_case.pause_song.PauseSongInputBoundary;
 import use_case.pause_song.PauseSongInputData;
-import use_case.pause_song.PauseSongInteractor;
-import use_case.play_song.PlaySongInputBoundary;
 import use_case.play_song.PlaySongInputData;
-import use_case.play_song.PlaySongInteractor;
 import use_case.prev_song.PrevSongInputBoundary;
 import use_case.prev_song.PrevSongInputData;
 import use_case.prev_song.PrevSongInteractor;
-import use_case.resume_song.ResumeSongInputBoundary;
 import use_case.resume_song.ResumeSongInputData;
-import use_case.resume_song.ResumeSongInteractor;
 import use_case.add_to_queue.AddToQueueInputBoundary;
 import use_case.add_to_queue.AddToQueueInputData;
 import use_case.add_to_queue.AddToQueueInteractor;
@@ -67,7 +54,7 @@ public class LoadSongsView extends JFrame {
     private JProgressBar SongProgressBar;
     private JPanel BackPanel;
     private JPanel MusicPlayPanel;
-    private JPanel ControlPanel;
+    private JPanel ControlsPanel;
     private JLabel CurrentSongField;
     private JPanel SongPanel;
     private JPanel AddToQueuePanel;
@@ -303,5 +290,10 @@ public class LoadSongsView extends JFrame {
         ControlsPanel.add(PreviousSongButton, 0);
         ControlsPanel.add(PlayPauseButton, 1);
         ControlsPanel.add(NextSongButton, 2);
+    }
+
+    private void createUIComponents() {
+        CoverPanel = new LoadAlbumView.ImagePanel("swiftify-frontend/src/pngs/taylor_swift.png");
+        setContentPane(CoverPanel);
     }
 }
