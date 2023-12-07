c59bc33399da194f0d6154e225146a58d6004a3c
jlowb <nederaalex@gmail.com>
2023-12-07 12:16:47 -0500

    added new entity tests, fixed data access tests
    (cherry picked from commit 6baac2059a2ffe7f8c6e305f1afa02e495548a1d)

--- swiftify-frontend/src/entity/Song.java
+++ swiftify-frontend/src/entity/Song.java
@@ -2,6 +2,10 @@ package entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * The Song class represents a music track with various attributes.
+ * It implements the Comparable interface to allow sorting based on the song ID.
+ */
 public class Song implements Comparable<Song> {
     private final int songID;
     private final String album;
@@ -10,7 +14,16 @@ public class Song implements Comparable<Song> {
     private final String s3Key;
     private final String title;
 
-    // Constructor
+    /**
+     * Constructs a new Song object with specified details.
+     *
+     * @param songID The unique identifier for the song.
+     * @param album The album to which the song belongs.
+     * @param duration The duration of the song in seconds.
+     * @param releaseYear The year when the song was released.
+     * @param s3Key The S3 key where the song file is stored.
+     * @param title The title of the song.
+     */
     public Song(@JsonProperty("SongID") int songID,
                 @JsonProperty("album") String album,
                 @JsonProperty("duration") int duration,
@@ -25,34 +38,69 @@ public class Song implements Comparable<Song> {
         this.title = title;
     }
 
+    /**
+     * Returns the song ID.
+     *
+     * @return The unique identifier of the song.
+     */
     public int getSongID() {
         return songID;
     }
 
+    /**
+     * Returns the album name of the song.
+     *
+     * @return The album name.
+     */
     public String getAlbum() {
         return album;
     }
 
+    /**
+     * Returns the duration of the song.
+     *
+     * @return The duration in seconds.
+     */
     public int getDuration() {
         return duration;
     }
 
+    /**
+     * Returns the release year of the song.
+     *
+     * @return The year of release.
+     */
     public int getReleaseYear() {
         return releaseYear;
     }
 
+    /**
+     * Returns the S3 key of the song.
+     *
+     * @return The S3 key where the song file is stored.
+     */
     public String getS3Key() {
         return s3Key;
     }
 
+    /**
+     * Returns the title of the song.
+     *
+     * @return The title of the song.
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Compares this song with another song based on song ID.
+     *
+     * @param other The Song object to be compared.
+     * @return A negative integer, zero, or a positive integer as this song ID
+     *         is less than, equal to, or greater than the specified song's ID.
+     */
     @Override
     public int compareTo(Song other) {
         return Integer.compare(this.songID, other.songID);
     }
 }
-
-
--- swiftify-frontend/test/EntityTest.java
+++ swiftify-frontend/test/EntityTest.java
@@ -1,64 +1,126 @@
-import entity.AddToQueueButton;
-import entity.Album;
-import entity.MusicQueue;
-import entity.Song;
+import interface_adapter.SongPlaybackState;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
+import static org.junit.Assert.*;
+import entity.*;
 import org.junit.jupiter.api.BeforeEach;
 
-import java.util.ArrayList;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
-import static org.junit.Assert.*;
-/*
 public class EntityTest {
-    public static class AddToQueueButtonTest {
 
-        @Test
-        public void testConstructorAndGetters() {
-            AddToQueueButton button = new AddToQueueButton("Test Song", 1);
+    public static class SongTest {
+        private Song song;
 
-            assertNotNull(button);
-            assertEquals("Test Song", button.getText());
-            assertEquals(1, button.getSongId());
+        @Before
+        public void setUp() {
+            song = new Song(1, "Album1", 300, 2020, "s3key1", "Title1");
         }
 
         @Test
-        public void testSetAndGetSongId() {
-            AddToQueueButton button = new AddToQueueButton("Test Song", 1);
-
-            assertEquals(1, button.getSongId());
-            button.setSongId(2);
-            assertEquals(2, button.getSongId());
+        public void testGetSongID() {
+            assertEquals(1, song.getSongID());
         }
 
+        @Test
+        public void testGetAlbum() {
+            assertEquals("Album1", song.getAlbum());
+        }
+
+        @Test
+        public void testGetDuration() {
+            assertEquals(300, song.getDuration());
+        }
+
+        @Test
+        public void testGetReleaseYear() {
+            assertEquals(2020, song.getReleaseYear());
+        }
+
+        @Test
+        public void testGetS3Key() {
+            assertEquals("s3key1", song.getS3Key());
+        }
+
+        @Test
+        public void testGetTitle() {
+            assertEquals("Title1", song.getTitle());
+        }
+
+        @Test
+        public void testCompareToEqual() {
+            Song sameSong = new Song(1, "Album2", 350, 2021, "s3key2", "Title2");
+            assertEquals(0, song.compareTo(sameSong));
+        }
+
+        @Test
+        public void testCompareToGreater() {
+            Song greaterSong = new Song(2, "Album2", 350, 2021, "s3key2", "Title2");
+            assertTrue(song.compareTo(greaterSong) < 0);
+        }
+
+        @Test
+        public void testCompareToLess() {
+            Song lesserSong = new Song(0, "Album0", 250, 2019, "s3key0", "Title0");
+            assertTrue(song.compareTo(lesserSong) > 0);
+        }
+
+        //@Test
+        //public void testNegativeDuration() {
+        //    assertThrows(IllegalArgumentException.class, () -> {
+        //        new Song(1, "Album", -300, 2020, "s3key", "Title");
+        //    });
+        //}
+
+        @Test
+        public void testLargeSongID() {
+            Song largeIDSong = new Song(Integer.MAX_VALUE, "Album", 300, 2020, "s3key", "Title");
+            assertEquals(Integer.MAX_VALUE, largeIDSong.getSongID());
+        }
+
+        @Test
+        public void testEmptyAlbumTitle() {
+            Song emptyAlbumSong = new Song(1, "", 300, 2020, "s3key", "Title");
+            assertEquals("", emptyAlbumSong.getAlbum());
+        }
     }
 
     public static class AlbumTest {
+        private Album album;
+        private List<Song> songList;
 
-        private Album testAlbum;
-        private List<Song> testSongs;
-
-        @BeforeEach
-        void setUp() {
-            // Create a sample list of songs for testing
-            testSongs = new ArrayList<>();
-            testSongs.add(new Song(1, "Test Album", 180, 2022, "test-key-1", "Test Song 1"));
-            testSongs.add(new Song(2, "Test Album", 200, 2022, "test-key-2", "Test Song 2"));
-
-            testAlbum = new Album("Test Album", testSongs);
+        @Before
+        public void setUp() {
+            Song song1 = new Song(1, "Album1", 300, 2020, "s3key1", "Title1");
+            Song song2 = new Song(2, "Album1", 350, 2021, "s3key2", "Title2");
+            songList = Arrays.asList(song1, song2);
+            album = new Album("Album1", songList);
         }
 
-        @org.junit.jupiter.api.Test
-        void testGetName() {
-            Assertions.assertEquals("Test Album", testAlbum.getName());
+        @Test
+        public void testGetName() {
+            assertEquals("Album1", album.getName());
         }
 
-        @org.junit.jupiter.api.Test
-        void testGetSongs() {
-            Assertions.assertNotNull(testAlbum.getSongs());
-            Assertions.assertEquals(2, testAlbum.getSongs().size());
+        @Test
+        public void testGetSongs() {
+            assertEquals(songList, album.getSongs());
+            assertEquals(2, album.getSongs().size());
+        }
+
+        @Test
+        public void testGetAlbumType() {
+            assertNull(album.getAlbumType());
+        }
+
+        @Test
+        public void testSetAndGetAlbumType() {
+            album.setAlbumType("Studio");
+            assertEquals("Studio", album.getAlbumType());
         }
     }
 
@@ -68,55 +130,148 @@ public class EntityTest {
 
         @Before
         public void setUp() {
-            // Set up a MusicQueue for testing
             musicQueue = new MusicQueue(false, 10, "Test Album");
         }
 
         @Test
-        public void testAdd() {
-            musicQueue.add(1);
-            musicQueue.add(2);
-            assertEquals(1, musicQueue.head.songID);
-            assertEquals(2, musicQueue.tail.songID);
-            assertFalse(musicQueue.shuffled());
+        public void testMusicQueueInitialization() {
+            assertFalse("Queue should be empty initially", musicQueue.isNotEmpty());
+            assertFalse("Shuffle should be initially off", musicQueue.shuffled());
+            assertEquals("Initial album name should match", "Test Album", musicQueue.getAlbumName());
+            assertEquals("Discography length should match", 10, musicQueue.getDiscographyLength());
         }
 
         @Test
-        public void testNext() {
+        public void testAddAndGetCurrentID() {
+            musicQueue = new MusicQueue(false, 10, "Test Album");
+            musicQueue.add(1);
+            assertEquals("Current song ID should be 1", 1, musicQueue.getCurrentID());
+        }
+
+        @Test
+        public void testNextOnEmptyQueue() {
+            musicQueue = new MusicQueue(false, 10, "Test Album");
+            musicQueue.next();
+            assertEquals("Next on empty queue should not change current song ID", -1, musicQueue.getCurrentID());
+        }
+
+        @Test
+        public void testPreviousOnEmptyQueue() {
+            musicQueue = new MusicQueue(false, 10, "Test Album");
+            musicQueue.previous();
+            assertEquals("Previous on empty queue should not change current song ID", -1, musicQueue.getCurrentID());
+        }
+
+        @Test
+        public void testAddSong() {
+            musicQueue.add(5);
+            assertEquals("Current song ID should be 5 after adding", 5, musicQueue.getCurrentID());
+        }
+
+        @Test
+        public void testNextSong() {
             musicQueue.add(1);
             musicQueue.add(2);
             musicQueue.next();
-            assertEquals(2, musicQueue.current.songID);
+            assertEquals("Current song ID should be 2 after next", 2, musicQueue.getCurrentID());
         }
 
         @Test
-        public void testPrevious() {
+        public void testPreviousSong() {
             musicQueue.add(1);
             musicQueue.add(2);
             musicQueue.next();
             musicQueue.previous();
-            assertEquals(1, musicQueue.current.songID);
+            assertEquals("Current song ID should revert to 1 after previous", 1, musicQueue.getCurrentID());
         }
 
         @Test
         public void testToggleShuffle() {
-            assertFalse(musicQueue.shuffled());
             musicQueue.toggleShuffle();
-            assertTrue(musicQueue.shuffled());
+            assertTrue("Shuffle should be on after toggling", musicQueue.shuffled());
+        }
+
+        @Test
+        public void testClearQueue() {
+            musicQueue = new MusicQueue(false, 10, "Test Album");
+            musicQueue.add(1);
+            musicQueue.add(2); // Add a second song to test if it's removed from the next position
+            musicQueue.clearQueue();
+            musicQueue.next(); // Try moving to the next song
+            assertEquals("There should be no next song after clearQueue", -1, musicQueue.getCurrentID());
         }
 
         @Test
         public void testIterator() {
             musicQueue.add(1);
             musicQueue.add(2);
+            Iterator<Integer> iterator = musicQueue.iterator();
+            assertTrue("Iterator should have next", iterator.hasNext());
+            assertEquals("First element should be 1", Integer.valueOf(1), iterator.next());
+        }
 
-            StringBuilder result = new StringBuilder();
-            for (int id : musicQueue) {
-                result.append(id).append(" ");
-            }
+        //@Test(expected = IllegalArgumentException.class)
+        //public void testAddNegativeSongId() {
+        //    musicQueue.add(-1);
+        //}
 
-            assertEquals("1 2 ", result.toString());
+        @Test
+        public void testDisplayQueue() {
+            musicQueue.add(1);
+            final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(outContent));
+            musicQueue.displayQueue();
+            assertTrue("Output should contain song ID 1", outContent.toString().contains("1"));
+            System.setOut(System.out);
+        }
+    }
+
+    public static class SongButtonTest {
+
+        private SongButton songButton;
+
+        @Before
+        public void setUp() {
+            Integer songId = 1;
+            String songName = "Test Song";
+            songButton = new SongButton(songId, songName);
+        }
+
+        @Test
+        public void testGetSongId() {
+            assertEquals("Song ID should be 1", Integer.valueOf(1), songButton.getSongId());
+        }
+
+        @Test
+        public void testGetSongName() {
+            assertEquals("Song name should be 'Test Song'", "Test Song", songButton.getSongName());
+        }
+
+        @Test
+        public void testButtonText() {
+            assertEquals("Button text should match song name", "Test Song", songButton.getText());
+        }
+    }
+
+    public static class AddToQueueButtonTest {
+
+        private AddToQueueButton queueButton;
+
+        @Before
+        public void setUp() {
+            Integer songId = 2;
+            String title = "Queue Song";
+            queueButton = new AddToQueueButton(title, songId);
+        }
+
+        @Test
+        public void testGetSongId() {
+            assertEquals("Song ID should be 2", Integer.valueOf(2), queueButton.getSongId());
+        }
+
+        @Test
+        public void testButtonText() {
+            assertEquals("Button text should match the title 'Queue Song'", "Queue Song", queueButton.getText());
         }
     }
 }
-*/
