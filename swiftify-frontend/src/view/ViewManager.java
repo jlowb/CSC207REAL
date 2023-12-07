1df988c55a52a89d46b13de66ddd4881958824bc
malaikamalik <malaika.malik@mail.utoronto.ca>
2023-12-07 18:49:04 -0500

    adding more javadocs- malaika

--- swiftify-frontend/src/view/ViewManager.java
+++ swiftify-frontend/src/view/ViewManager.java
@@ -7,14 +7,29 @@ import javax.swing.*;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
+/**
+ * Manages views and responds to changes in the associated ViewManagerModel.
+ */
+
 public class ViewManager implements PropertyChangeListener {
 
     private final ViewManagerModel viewManagerModel;
+    /**
+     * Constructs a new ViewManager with the specified ViewManagerModel.
+     *
+     * @param viewManagerModel The model responsible for managing views.
+     */
 
     public ViewManager(ViewManagerModel viewManagerModel) {
         this.viewManagerModel = viewManagerModel;
         this.viewManagerModel.addPropertyChangeListener(this);
     }
+    /**
+     * Called when a property of the associated ViewManagerModel changes.
+     * This method creates and displays a new view based on the updated ViewModel.
+     *
+     * @param evt The PropertyChangeEvent containing information about the change.
+     */
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
