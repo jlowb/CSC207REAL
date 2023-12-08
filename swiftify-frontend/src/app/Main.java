f854c60de253efd3dd70a7202947eb2aff3038e3
malaikamalik <malaika.malik@mail.utoronto.ca>
2023-12-07 19:00:21 -0500

    adding main javadocs- malaika

--- swiftify-frontend/src/app/Main.java
+++ swiftify-frontend/src/app/Main.java
@@ -3,13 +3,23 @@ package app;
 import interface_adapter.ViewManagerModel;
 import view.MainView;
 import view.ViewManager;
+/**
+ * The main class for the Swiftify application.
+ */
 
 public class Main {
+    /**
+     * The entry point of the application.
+     *
+     */
 
     public static void main(String[] args) {
+        // Create a ViewManagerModel to manage views
         ViewManagerModel viewManagerModel = new ViewManagerModel();
+        // Create a ViewManager to respond to changes in the ViewManagerModel
         new ViewManager(viewManagerModel);
 
+        // Create the main view and associate it with a LoadAlbumsController
         MainView mainView = new MainView(LoadAlbumsUseCaseFactory.createLoadAlbumsController(viewManagerModel));
     }
 }
